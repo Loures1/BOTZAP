24dc34471776b95ed411fdfec9c2c4bc78cf385b
Loures1 <lucasantonio.loures@gmail.com>
2022-05-20 13:20:27 -0300

    Pesquisa e seleciona o contatdo

--- zapbot.py
+++ zapbot.py
@@ -9,10 +9,24 @@ options = ChromeOptions()
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 driver.get("https://web.whatsapp.com/")
+not_login = True 
 
-while len(driver.find_elements_by_id("side")) < 1:
+while len(driver.find_elements(By.XPATH, '//*[@id="side"]')) == False:
+    if not_login == True:
+        print('Aguardando Login')
+        not_login = False
+    
     time.sleep(1)
-        
-        
 
+time.sleep(3)
 
+if len(driver.find_elements(By.XPATH, '//*[@id="side"]')) == True:
+    print('Logado')
+    time.sleep(1)
+    element_search = driver.find_element(By.XPATH, '//*[@id="side"]/div[1]/div/label/div/div[2]')
+    element_search.click()
+    pessoa = 'Isa'
+    element_search.send_keys(pessoa)
+    time.sleep(1)
+    element_contato = driver.find_element(By.XPATH, f"//span[@title='{pessoa}']")
+    element_contato.click()
